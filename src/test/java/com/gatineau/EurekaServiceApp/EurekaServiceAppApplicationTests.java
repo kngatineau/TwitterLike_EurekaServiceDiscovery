be4020e0b5df0d04a03d4dd02d7d4303b19a4559
kngatineau <gati0010@algonquinlive.com>
2022-09-14 17:21:50 -0600

    initial commit

---
+++ src/test/java/com/gatineau/EurekaServiceApp/EurekaServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatineau.EurekaServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
